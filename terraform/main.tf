adae3265eaaeb8d012f229867f1b8475904fa3c6
akira satake <t06418as@gmail.com>
2018-11-18 09:00:07 +0900

    modify terraform variables about region

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {}
 
 resource "azurerm_resource_group" "functions_sample" {
   name     = "functions-sample-resource-group"
-  location = "japaneast"
+  location = "${var.main_region}"
 
   tags {
     environment = "${var.environment}"
